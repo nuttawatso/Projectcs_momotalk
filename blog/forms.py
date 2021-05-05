dcf76c1b44165a135d03943825e5092ac2e0a8cc
nuttawatso <nuttawat.so.60@ubu.ac.th>
2021-05-05 13:47:36 +0700

    05-05

--- blog/forms.py
+++ blog/forms.py
@@ -8,20 +8,31 @@ class PostsForm(forms.ModelForm):
         widgets = {}
         model = Posts
         path = forms.CharField(required=False)
-        fields = {'title','category','description' , 'picture' ,'pic_name' }
-        widgets = {'picture': forms.HiddenInput(),'pic_name': forms.HiddenInput()}
+        fields = {'title','category','description','picture','pic_name' }
+        widgets = {
+            'picture': forms.HiddenInput(),
+            'pic_name': forms.HiddenInput(),
+            'title' : forms.TextInput(attrs={'style': 'border-color:darkgoldenrod; border-radius: 35px;', 'placeholder':'หัวข้อโพสต์'})
+            
+            }
         labels = {
-            'title':'หัวข้อ',
-            'category':'หมวดหมู่',
-            'description':'รายละเอียด',
+            'title':'',
+            'category':'',
+            'description':'',
+            
         }
-    description = forms.CharField(label ="เนื้อหาข้อความ", widget = forms.Textarea(
+        
+    description = forms.CharField(label ="", widget = forms.Textarea(
     attrs ={
         'class':'form-control',
-        'placeholder':'',
+        'placeholder':'รายละเอียด',
         'rows':3,
        
     }))
+  
+   
+   
+
 
 
 class CommentForm(forms.ModelForm):
--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,12 @@ from django.contrib import messages
 from django.shortcuts import  redirect, get_object_or_404 , HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
-import random
 from random_word import RandomWords
 from django.contrib.auth.decorators import login_required
 import os
 from .forms import CommentForm
-
+import requests
+import random
 
     
 def like(request):
@@ -22,11 +22,11 @@ def like(request):
         exist=0
         id = int(request.POST.get('postid'))
         post = get_object_or_404(Posts, post_id=id)
-        print('trttt',request.POST.get('postid'),post)
+        # print('trttt',request.POST.get('postid'),post)
         if post.liked.filter(id=request.user.id).exists():
             post.liked.remove(request.user.id)
             post.like_count -= 1
-            print(post.liked,post.like_count)
+            # print(post.liked,post.like_count)
             result = post.like_count
             exist=0
             post.save()
@@ -34,27 +34,24 @@ def like(request):
             post.liked.add(request.user.id)
             post.like_count += 1
             result = post.like_count
-            print(post.like_count)
+            # print(post.like_count)
             exist=1
             post.save()
-
         return JsonResponse({'result': result,'postId': id,'exist':exist })
 
 def favourite_add(request):
-    if request.POST.get('action') == 'post':
-        
+    if request.POST.get('action') == 'post':    
         id = int(request.POST.get('postid'))
         print('trttt',request.POST.get('postid'))
-
         post = get_object_or_404(Posts, post_id=id)
         print('post',post)
         if post.favourites.filter(id=request.user.id).exists():
             post.favourites.remove(request.user.id)
+            exist=0
         else:
             post.favourites.add(request.user.id)
-    
+            exist=1
         return JsonResponse({'postId': id })
-    
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
     
@@ -74,61 +71,51 @@ def favourties(request,id=0):
 
 def post_comment(request,id=0):
     post = get_object_or_404(Posts, post_id=id)
-
     all_comments = Comment.objects.filter(post_id=id).order_by("-timestamp")
     if request.method == 'POST':
-        foo = os.listdir('blog/static/media')
-        randomName = RandomWords().get_random_word()
+        foo = os.listdir('blog/static/media')      
+        word_site = "https://www.usna.edu/Users/cs/roche/courses/s15si335/proj1/files.php%3Ff=names.txt&downloadcode=yes"
+        response = requests.get(word_site)
+        WORDS = response.content.splitlines()
+        randomName = random.choice(WORDS).decode("utf-8")
         randompicture = random.choice(foo)
         cf=CommentForm(request.POST or None)
         if cf.is_valid():
             content=request.POST.get('content')
-            url = "https://api.aiforthai.in.th/bully"
-            
-            data = {'text':content}
-            
+            url = "https://api.aiforthai.in.th/bully"   
+            data = {'text':content}            
             headers = {   
                 'Apikey': "vM0LY7YAGqM2DhFbJHI3ON5mmg71vs9K",
-                }
-            
+                }          
             response = requests.post(url, data=data, headers=headers)
-            print('response')
-            print(response.json())
+            # print('response')
+            # print(response.json())
             if (response.json()['bully_type'] != 0):
-                
                 messages.warning(request,response.json()['bully_word']+['เป็นคำไม่สุภาพ กรุณาแก้ไข'])            
                 return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-            else:
-                    
+            else:   
                 if request.user.id == post.user_id:
                     comment=Comment.objects.create(post=post,user=request.user,content=content,picture=post.picture,pic_name= post.pic_name)
-                    print('if')
+                    # print('if')
                     comment.save()
-                    
-
                 elif Comment.objects.filter(user_id=request.user.id).filter(post_id=id).exists():
-                    print(Comment.objects.filter(user_id=request.user.id).filter(post_id=id)[0].picture)
+                    # print(Comment.objects.filter(user_id=request.user.id).filter(post_id=id)[0].picture)
                     chk_user = Comment.objects.filter(user_id=request.user.id).filter(post_id=id)[0]
-                    print('elif',chk_user)
+                    # print('elif',chk_user)
                     comment=Comment.objects.create(post=post,user=request.user,content=content,picture=chk_user.picture,pic_name= chk_user.pic_name)
-                    comment.save()
-        
+                    comment.save()       
                 else:
                     comment=Comment.objects.create(post=post,user=request.user,content=content,picture=str(randompicture),pic_name= str(randomName))
                     print('el')
-                    comment.save()
-                    
+                    comment.save()       
                 return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-     
     else:
         cf=CommentForm()
-
     return render(request,'post_comment.html',{"comments":cf,'all_comments':all_comments,'post':post})
 
 def del_comment(request,id=0):
     if request.method == "GET" and id!=0:
-        print('testttt')
+        # print('testttt')
         post = Comment.objects.get(id=id)
         post.delete()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
@@ -136,8 +123,7 @@ def del_comment(request,id=0):
 def catagory_select(request,  catagory_select=0):
     list_category = ['all','ทั่วไป','ความรัก','ปรึกษา','การเรียน','การเมือง','ดนตรี','นิยาย/ซีรี่ย์']
     foo = os.listdir('blog/static/media')
-
-    print(list_category[int(catagory_select)])
+    # print(list_category[int(catagory_select)])
     catagory_select = int(catagory_select)
     if catagory_select != 0:
         posts = Posts.objects.filter(category=list_category[catagory_select]).order_by('-post_id')
@@ -159,7 +145,10 @@ def post_management(request, id=0, catagory_select=""):
             else :
                 posts = Posts.objects.all().order_by('-post_id')
                 hotPosts = Posts.objects.all().order_by('-like_count')
-                randomName = RandomWords().get_random_word()
+                word_site = "https://www.usna.edu/Users/cs/roche/courses/s15si335/proj1/files.php%3Ff=names.txt&downloadcode=yes"
+                response = requests.get(word_site)
+                WORDS = response.content.splitlines()
+                randomName = random.choice(WORDS).decode("utf-8")
                 randompicture = random.choice(foo)
                 form = PostsForm()
                 form = PostsForm(initial={'picture': str(randompicture),'pic_name': str(randomName)})
@@ -167,7 +156,7 @@ def post_management(request, id=0, catagory_select=""):
 
             return render(request,'index.html', {"posts":posts,'form':form,"hotPosts":hotPosts})
         else:
-            print('testttt')
+            # print('testttt')
             # delete post
             post = Posts.objects.get(post_id=id)
             post.delete()
@@ -177,24 +166,21 @@ def post_management(request, id=0, catagory_select=""):
         form = PostsForm(request.POST)
         if form.is_valid():
             url = "https://api.aiforthai.in.th/bully"
-            text = form.instance
-            
-            data = {'text':text}
-            
+            text = form.instance           
+            data = {'text':text}         
             headers = {   
                 'Apikey': "vM0LY7YAGqM2DhFbJHI3ON5mmg71vs9K",
-                }
-            
+                }     
             response = requests.post(url, data=data, headers=headers)
-            print('response')
-            print(response.json())
+            # print('response')
+            # print(response.json())
             if (response.json()['bully_type'] != 0):
-                print(response.json()['bully_word'] , 'เป็นคำไม่สุภาพกรุณาพิมพ์ให้สุภาพ')            
+                # print(response.json()['bully_word'] , 'เป็นคำไม่สุภาพกรุณาพิมพ์ให้สุภาพ')            
                 messages.warning(request,response.json()['bully_word']+[': เป็นคำไม่เหมาะสม กรุณาแก้ไข'])              
                 return redirect('')
             else:
-                print('บันทึกสำเร็จ')
-                print(response.json())
+                # print('บันทึกสำเร็จ')
+                # print(response.json())
                 form.save()
                 messages.success(request,'บันทึกโพสต์สำเร็จ')
                 return redirect('')
@@ -205,28 +191,25 @@ def post_update(request, id=0):
         form = PostsForm(instance=post)
         return render(request,'updatepost.html',{'form':form,'id':id})
     elif request.method == "POST":
-        print(id,'edit')
+        # print(id,'edit')
         post = Posts.objects.get(post_id=id)
         form = PostsForm(request.POST, instance=post)
-        print('from ', form.errors)
+        # print('from ', form.errors)
         if form.is_valid():
             url = "https://api.aiforthai.in.th/bully"
             text = form
-            print('check form')
-            
-            data = {'text':text}
-            
+            # print('check form')
+            data = {'text':text}           
             headers = {   
                 'Apikey': "vM0LY7YAGqM2DhFbJHI3ON5mmg71vs9K",
-                }
-            
+                } 
             response = requests.post(url, data=data, headers=headers)
             if (response.json()['bully_type'] != 0):
-                print('กรุณาพิมพ์ให้สุภาพ')
+                # print('กรุณาพิมพ์ให้สุภาพ')
                 messages.warning(request,response.json()['bully_word'])  
                 return redirect('')          
             else:
-                print('บันทึกสำเร็จ')
-                print(response.json())
+                # print('บันทึกสำเร็จ')
+                # print(response.json())
                 form.save() 
         return redirect('')
